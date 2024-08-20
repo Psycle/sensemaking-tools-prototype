4447f1e9696ff2c8a56f6af65e87177ca3422231
alyssachvasta <achvasta@google.com>
2024-08-20 20:59:46 +0000

    Run formatter
    Change-Id: I9b40daa83686dfdba177bff402210d1b9eb1ce02

--- vertex_lib.ts
+++ vertex_lib.ts
@@ -59,24 +59,23 @@ const generativeJsonModel = vertex_ai.getGenerativeModel(json_model_spec);
 
 // Topic schema (TBC... doesn't work yet; see GH issue below)
 const topics_schema = {
-  "type": "array",
-  "items": {
-    "type": "object",
-    "properties": {
-      "name": {"type": "string"},
-      "subtopics": {
-        "type": "array",
-        "items": {
-          "type": "object",
-          "properties": {
-            "name": {"type": "string"}
-          }
-        }
-      }
-    }
-  }
-}
-
+  type: "array",
+  items: {
+    type: "object",
+    properties: {
+      name: { type: "string" },
+      subtopics: {
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            name: { type: "string" },
+          },
+        },
+      },
+    },
+  },
+};
 
 /**
  * Combines the data and instructions into a prompt to send to Vertex.
@@ -96,15 +95,13 @@ function getRequest(instructions: string, data: string[]) {
   };
 }
 
-
 class FailedExecutionError extends Error {
-  constructor(public response: any, message?: string)  {
+  constructor(public response: any, message?: string) {
     super(message || "An error occurred calling Vertex");
     this.name = "VertexAIExecutionError";
     this.response = response;
   }
 }
-
 
 /**
  * Lower level protocol for sending a set of instructions to an llm with
@@ -126,20 +123,16 @@ export async function executeRequest(
   }
 }
 
-
 /**
  * Utility function for sending a set of instructions to an LLM with comments,
  * and returning the results as JSON.
  */
-async function generateJSON(
-  instructions: string,
-  comments: string[]
-) {
+async function generateJSON(instructions: string, comments: string[]) {
   /**
    * this doesn't work yet, because of https://github.com/googleapis/nodejs-vertexai/issues/392
    */
   //const response = await generativeModel.generateContent({
-    //responseSchema: topics_schema // or is it response_schema?
+  //responseSchema: topics_schema // or is it response_schema?
   //});
 
   const req = getRequest(instructions, comments);
@@ -156,30 +149,40 @@ async function generateJSON(
   }
 }
 
-
-interface Topic  {
+interface Topic {
   name: string;
   subtopics?: Topic[];
 }
 
 export async function learnTopics(
   comments: string[],
-  {depth = 1, parentTopics}: {depth?: number, parentTopics?: string[]}
+  { depth = 1, parentTopics }: { depth?: number; parentTopics?: string[] }
 ): Promise<Topic[]> {
-  let instructions = "Identify a " + depth + "-tiered hierarchical topic modeling of the following comments, and return the results as ";
+  let instructions =
+    "Identify a " +
+    depth +
+    "-tiered hierarchical topic modeling of the following comments, and return the results as ";
   if (depth == 1) {
     instructions += "an array of strings.";
   } else if (depth == 2) {
-    instructions += "an array of objects with keys name and subtopics, where subtopics points to an array strings (no nested objects).";
+    instructions +=
+      "an array of objects with keys name and subtopics, where subtopics points to an array strings (no nested objects).";
   }
   if (parentTopics !== undefined) {
-    instructions += " The top level topics should be: " + JSON.stringify(['Infrastructure & Development', 'Social & Community Issues', 'Economy & Employment', 'Governance & Policy']) + ".";
+    instructions +=
+      " The top level topics should be: " +
+      JSON.stringify([
+        "Infrastructure & Development",
+        "Social & Community Issues",
+        "Economy & Employment",
+        "Governance & Policy",
+      ]) +
+      ".";
     console.log(instructions);
   }
   const response = await generateJSON(instructions, comments);
   return response;
 }
-
 
 /**
  * Summarizes the comments using a LLM on Vertex.
@@ -199,23 +202,19 @@ export async function summarize(
   }
 }
 
-
 /**
  * Extracts topics from the comments using a LLM on Vertex AI.
  * @param comments The comments data for topic modeling
  * @returns: The LLM's topic modeling.
  */
-export async function getTopics(
-  comments: string[]
-): Promise<string> {
-  const topicsResponse = await learnTopics(comments, {depth: 1});
+export async function getTopics(comments: string[]): Promise<string> {
+  const topicsResponse = await learnTopics(comments, { depth: 1 });
   if (topicsResponse instanceof FailedExecutionError) {
     throw topicsResponse;
   } else {
     return JSON.stringify(topicsResponse);
   }
 }
-
 
 /**
  * Categorize the comments by topics using a LLM on Vertex.
@@ -224,8 +223,8 @@ export async function getTopics(
  * @returns: The LLM's categorization.
  */
 export async function categorize(
-    instructions: string,
-    comments: string[]
+  instructions: string,
+  comments: string[]
 ): Promise<string> {
   const categorizeResponse = await executeRequest(instructions, comments);
   if (categorizeResponse instanceof FailedExecutionError) {
