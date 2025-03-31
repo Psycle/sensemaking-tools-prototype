72a986065660c2c59fb3d66cf4fa7288d75d08c2
Stuart Messenger <stuart.messenger@psycle.com>
2025-03-31 12:30:57 +0100

    debug

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'gh-pages'),
   },
   plugins: [
     // Copy index.html to /dist
